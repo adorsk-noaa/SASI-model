5848f9d40c090555d9f187edc88ded6ccb32b1ea
adorsk <adorsk>
2012-06-26 10:53:40 -0400

    added gear code mappings

--- sasi_model/conf.py
+++ sasi_model/conf.py
@@ -1,3 +1,11 @@
 conf = {
-	'verbose': True
+	'verbose': True,
+    'dry_code_to_gear_category':  {
+        'GC1': 'Trawl',
+        'GC2': 'Scallop',
+        'GC3': 'Dredge',
+        'GC4': 'Longline',
+        'Gillnet': 'GC5',
+        'Trap': 'GC6'
+    }
 }
